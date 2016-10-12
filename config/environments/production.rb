d661b60a94eea15924e0747cb1893db0ac9e51bd
rdalin82 <Robert.Dalin@gmail.com>
2016-10-11 21:41:48 -0500

    domain for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'rdalin-movietheater.herokuapp.com',
     user_name:            ENV['EMAIL'],
     password:             ENV['PASSWORD'],
     authentication:       'plain',
