f0fcdcd4010900c2b210d3b0971880b79f2ecc6a
rdalin82 <Robert.Dalin@gmail.com>
2016-10-11 21:22:59 -0500

    prep for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            EMAIL,
-    password:             PASSWORD,
+    user_name:            ENV['EMAIL]',
+    password:             ENV['PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true 
    }
