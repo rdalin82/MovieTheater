fbc53359d7fde2739714d617822d00500e2296d3
rdalin82 <Robert.Dalin@gmail.com>
2016-10-11 21:36:33 -0500

    prep for heroku change to migration

--- db/migrate/20161010162927_removetheater.rb
+++ db/migrate/20161010162927_removetheater.rb
@@ -2,6 +2,6 @@ class Removetheater < ActiveRecord::Migration
   def change
     remove_column :auditoria, :theater_id
     drop_table :theaters 
-    drop_table :auditoriums_in_theater
+    # drop_table :auditoriums_in_theater
   end
 end
